86b13c9e22d09184ecb861c9259f5f82fa7b629c
Frank <frank.ankch17@gmail.com>
2023-05-02 17:57:57 +0800

    add xlsx version

---
+++ xlsx/ts2xlsx.py
@@ -0,0 +1,52 @@
+import openpyxl
+from lxml import etree
+
+# 讀取XML檔案並獲取根元素
+tree = etree.parse('test.ts')
+root = tree.getroot()
+
+# 創建一個新的XLSX工作簿
+workbook = openpyxl.Workbook()
+worksheet = workbook.active
+
+# 添加標題行
+headers = ["content_name", "location_file", "location_line", "source", "translation"]
+for col_num, header in enumerate(headers, 1):
+    worksheet.cell(row=1, column=col_num).value = header
+
+# 遍歷<context>標籤並將資料寫入XLSX
+row_num = 2
+for context in root.findall("context"):
+    content_name = context.find("name")
+    content_name = content_name.text if content_name is not None else ""
+
+    # 遍歷<message>標籤
+    for message in context.findall("message"):
+        source = message.find("source")
+        source = source.text if source is not None else ""
+
+        translation = message.find("translation")
+        translation = translation.text if translation is not None else ""
+
+        locations = message.findall("location")
+        if locations:
+            for location in locations:
+                location_file = location.get("filename") if location is not None else ""
+                location_line = location.get("line") if location is not None else ""
+
+                # 寫入XLSX行
+                for col_num, cell_value in enumerate([content_name, location_file, location_line, source, translation], 1):
+                    worksheet.cell(row=row_num, column=col_num).value = cell_value
+                row_num += 1
+        else:
+            # 如果沒有<location>標籤，則使用空字符串
+            location_file = ""
+            location_line = ""
+
+            # 寫入XLSX行
+            for col_num, cell_value in enumerate([content_name, location_file, location_line, source, translation], 1):
+                worksheet.cell(row=row_num, column=col_num).value = cell_value
+            row_num += 1
+
+# 保存XLSX檔案
+workbook.save("output.xlsx")
---
+++ xlsx/xlsx2ts.py
@@ -0,0 +1,58 @@
+import openpyxl
+from lxml import etree
+
+# 读取XML文件并获取根元素
+parser = etree.XMLParser(strip_cdata=False)
+tree = etree.parse('test.ts', parser)
+root = tree.getroot()
+
+# 读取XLSX文件
+workbook = openpyxl.load_workbook("output.xlsx")
+worksheet = workbook.active
+
+# 遍历XLSX中的每一行
+for row_num in range(2, worksheet.max_row + 1):
+    content_name = worksheet.cell(row=row_num, column=1).value
+    location_file = worksheet.cell(row=row_num, column=2).value
+    location_line = worksheet.cell(row=row_num, column=3).value
+    source_text = worksheet.cell(row=row_num, column=4).value
+    translation_text = worksheet.cell(row=row_num, column=5).value
+
+    # 找到匹配的<context>标签
+    context_match = None
+    for context in root.findall("context"):
+        name = context.find("name")
+        if name is not None and name.text == content_name:
+            context_match = context
+            break
+
+    if context_match is None:
+        continue
+
+    # 找到匹配的<message>标签
+    message_match = None
+    for message in context_match.findall("message"):
+        source = message.find("source")
+        if source is not None and source.text == source_text:
+            for location in message.findall("location"):
+                if (location.get("filename") == location_file and
+                        location.get("line") == location_line):
+                    message_match = message
+                    break
+
+        if message_match is not None:
+            break
+
+    if message_match is None:
+        continue
+
+    # 更新<translation>标签的内容
+    translation = message_match.find("translation")
+    if translation is not None:
+        translation.text = translation_text
+
+# 将更新后的XML树写回文件，添加xml声明和doctype
+with open("updated_xml_file.xml", "wb") as f:
+    f.write(b'<?xml version="1.0" encoding="utf-8"?>\n')
+    f.write(b'<!DOCTYPE TS>\n')
+    f.write(etree.tostring(root, encoding="utf-8"))
